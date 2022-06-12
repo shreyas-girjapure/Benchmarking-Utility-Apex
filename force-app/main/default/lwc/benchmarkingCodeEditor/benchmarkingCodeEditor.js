1cbbd482ed778946459116b430b368b3597891b2
Shreyas Girjapure <shreyas_girjapure@persistent.com>
2022-06-13 01:26:41 +0530

    base

---
+++ force-app/main/default/lwc/benchmarkingCodeEditor/benchmarkingCodeEditor.js
@@ -0,0 +1,28 @@
+import { LightningElement } from "lwc";
+
+function stripHtmlFromRichInnerText(htmlString) {
+  let doc = new DOMParser().parseFromString(htmlString, "text/html");
+  return doc.body.textContent || "";
+}
+export default class BenchmarkingCodeEditor extends LightningElement {
+  allowedFormats = ["size", "indent", "align", "clean", "code"];
+  referenceEditorValue;
+  targetEditorValue;
+
+  finalApexString;
+  handleReferenceEditorChange(event) {
+    this.referenceEditorValue = event.target.value;
+    console.log("ref" + stripHtmlFromRichInnerText(this.referenceEditorValue));
+  }
+  handleTargetEditorChange(event) {
+    this.targetEditorValue = event.target.value;
+    console.log("target" + stripHtmlFromRichInnerText(this.targetEditorValue));
+  }
+  excecuteApexCode() {
+    this.finalApexString = this.referenceEditorValue + this.targetEditorValue;
+
+    if (this.finalApexString) {
+      console.log("in final string check");
+    }
+  }
+}
